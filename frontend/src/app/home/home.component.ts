b2de0305626c807517bf1584c19cb62e06afa82e
João Souza <marceloamaral201924@gmail.com>
2025-03-18 09:31:53 -0400

    feat: new function saveName() and constructor added using formerBuilder

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public names: string[] = [];
+  public inputName = "";
+  public userForm: FormGroup;
+  public isRed = true;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      'name': ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  public saveName() {
+    let newName = this.userForm.get('name')?.value;
+    if (this.userForm.valid) {
+      this.names.push(newName);
+      this.userForm.reset();
+    }
+  }
 }
